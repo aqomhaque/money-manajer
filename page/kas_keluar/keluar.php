5975df31355ed219a1d6b371b7a6e358acb36f6f
aqomhaque <haquemlg1987@gmail.com>
2019-11-13 09:12:41 +0700

    masih stuck pada penambahan data dan edit untuk pengeluaran :(

--- page/kas_keluar/keluar.php
+++ page/kas_keluar/keluar.php
@@ -1,1 +1,221 @@
-halaman keluar.php
+ 
+ <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                             Data Kas Keluar
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-kas">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Kode</th>
+                                            <th>Tanggal</th>
+                                            <th>Keterangan</th>
+                                            <th>Jumlah</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    <?php  
+
+                                        $no = 1;
+                                        
+                                        $sql = $koneksi->query("select * from tbl_kas where jenis = 'keluar' ");
+                                        while ($data=$sql->fetch_assoc()) {
+                                                
+                                        
+                                     ?>
+                                        <tr class="odd gradeX">
+                                            <td><?php echo $no++; ?></td>
+                                            <td><?php echo $data['kode']; ?></td>
+                                            <td><?php echo date('d F Y', strtotime($data['tgl'])); ?></td>
+                                            <td><?php echo $data['keterangan']; ?></td>
+                                            <td align="right"><?php echo"Rp. " .number_format($data['keluar']).",- "; ?></td>
+                                            <td>
+                                                <a id="edit_data" data-toggle="modal" data-target="#edit" 
+                                                data-id="<?php echo $data['kode'] ?>" 
+                                                data-ket="<?php echo $data['keterangan']?>" 
+                                                data-tgl="<?php echo $data['tgl'] ?>" 
+                                                data-jml="<?php echo $data['jumlah']?>"
+                                                class="btn btn-info"> <i class="fa fa-edit"></i>Edit</a>
+
+                                                <a onclick="return confirm('Apakah anda yakin akan menghapus data ini????')" href="?page=masuk&aksi=hapus&id=<?php echo $data['kode'];?>" class="btn btn-danger"> <i class="fa fa-trash"></i>Hapus</a>
+                                            </td>
+                                        </tr>
+
+                                        <?php 
+                                            $total=$total+$data['keluar'];
+                                            }
+                                         ?>
+
+                                    </tbody>
+                                        <tr>
+                                            <th colspan="4" style="text-align: center; font-size: 17px"> Total Kas Keluar</th>
+                                            <th style="text-align: right; font-size: 17px"><?php echo"Rp. " .number_format($total ). ",- "; ?></th>
+                                        </tr>
+                                </table>
+                            </div>
+
+                            <!-- PopUP Tambah Data -->
+                                <div class="panel-body">
+                                    <button class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">
+                                      Tambah Data 
+                                    </button>
+                                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                    <h4 class="modal-title" id="myModalLabel">Form Tambah Data</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form role="form" method="POST">
+                                                        <div class="form-group">
+                                                            <label>Kode</label>
+                                                            <input class="form-control" name="kode" placeholder="Masukkan Kode" />
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label>Keterangan</label>
+                                                            <textarea class="form-control" rows="3" name="ket"></textarea>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label>Tanggal</label>
+                                                            <input class="form-control" type="date" name="tgl" placeholder="Pilih Tanggal" />
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label>Jumlah</label>
+                                                            <input class="form-control" type="number" name="jml" placeholder="Masukkan Nominal" />
+                                                        </div>
+
+                                                        <p class="help-block">Perhatikan! Isi Data dengan Benar!</p>
+                                                   
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                                                    <input type="submit" name="simpan" value="Simpan" class=" btn btn-primary">
+                                                </div>
+                                                    
+                                                </div>
+                                                </form>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <?php 
+                                    if (isset($_POST['simpan'])) {
+
+                                        $kode = $_POST['kode'];
+                                        $ket = $_POST['ket'];
+                                        $tgl = $_POST['tgl'];
+                                        $jml = $_POST['jml'];
+                                        
+                                        $sql = $koneksi->query("insert into tbl_kas (kode, keterangan, tgl, jumlah, jenis, keluar)values('$kode','$ket','$tgl', 0 , 'keluar', '$jml')");
+                                        
+                                        if ($sql) {
+                                            ?>
+                                                <script type="text/javascript">
+                                                    alert("Simpan Data Berhasil!");
+                                                    window.location.href="?page=keluar";
+                                                </script>
+                                            <?php
+                                        }
+                                    }
+
+                                 ?>
+                         <!-- Akhir PopUP Tambah Data-->
+
+                         <!-- Halaman Ubah Data-->
+                         <div class="panel-body">
+                                    <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" id="modal_edit">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                    <h4 class="modal-title" id="myModalLabel">Form Edit Data</h4>
+                                                </div>
+                                                <div class="modal-body" id="modal_edit">
+                                                    <form role="form" method="POST">
+                                                        <div class="form-group">
+                                                            <label>Kode</label>
+                                                            <input class="form-control" name="kode" placeholder="Masukkan Kode" id="kode" readonly />
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label>Keterangan</label>
+                                                            <textarea class="form-control" rows="3" name="ket" id="ket"></textarea>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label>Tanggal</label>
+                                                            <input class="form-control" type="date" name="tgl" placeholder="Pilih Tanggal" id="tgl" />
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label>Jumlah</label>
+                                                            <input class="form-control" type="number" name="jml" placeholder="Masukkan Nominal" id="jml" />
+                                                        </div>
+
+                                                        <p class="help-block">Perhatikan! Edit dengan teliti!</p>
+                                                   
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                                                    <input type="submit" name="ubah" value="Ubah" class=" btn btn-primary">
+                                                </div>
+                                                    
+                                                </div>
+                                                </form>
+                                                <!-- Script PHP untuk Ubah / Update Data -->
+                                                    <?php 
+                                                        if (isset($_POST['ubah'])) {
+                                                            
+                                                            $kode = $_POST['kode'];
+                                                            $ket = $_POST['ket'];
+                                                            $tgl = $_POST['tgl'];
+                                                            $jml = $_POST['jml'];
+                                                            
+                                                            $sql = $koneksi->query("update tbl_kas set keterangan = '$ket', tgl = '$tgl', jumlah=0, jenis='keluar', keluar = '$jml' where kode = '$kode'");
+                                                             if ($sql) {
+                                                                ?>
+                                                                    <script type="text/javascript">
+                                                                        alert("Ubah Data Berhasil!");
+                                                                        window.location.href="?page=keluar";
+                                                                    </script>
+                                                                <?php
+                                                            }
+                                                        }
+
+                                                     ?>
+                                                     <!-- Akhir Script PHP untuk Ubah / Update Data -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--Script Tambahan .js-->
+                                <script src="assets/js/jquery-1.10.2.js"></script>
+                                <script type="text/javascript">
+                                $(document).on("click","#edit_data",function(){
+                                
+                                    var kode = $(this).data('id');
+                                    var ket = $(this).data('ket');
+                                    var tgl = $(this).data('tgl');
+                                    var jml = $(this).data('jml');
+
+                                    $("#modal_edit #kode").val(kode);
+                                    $("#modal_edit #ket").val(ket);
+                                    $("#modal_edit #tgl").val(tgl);
+                                    $("#modal_edit #jml").val(jml);
+                                })
+
+                                </script>
+                                
+                                
+
+                         <!-- Akhir Halaman Ubah Data-->
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
